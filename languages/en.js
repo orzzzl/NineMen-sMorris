d89554931b6589e9565d83b830502c22fa06db18
Zeleng Zhuang <zhuangzeleng19920731@gmail.com>
2015-05-05 23:07:26 -0400

    add helper

--- languages/en.js
+++ languages/en.js
@@ -1,9 +1,11 @@
 window.angularTranslationsLoaded('en',{
-"RULES_OF_BANQI":"Rules of Banqi",
-"RULES_SLIDE1":"The ranking goes as follows: general, advisor, elephant, chariot, horse, soldier. Only pieces of equal or lower rank may be captured, with one exception. The one exception concerns generals and soldiers: the general cannot capture soldiers, and soldiers can capture the general.",
-"RULES_SLIDE2":"Except for the cannon, pieces capture with the same motion as for movement: one square up, down, left, or right.",
-"RULES_SLIDE3":"The cannon is not included in the ranking because it is exceptional: it captures in an unusual way, it can capture a piece of any rank, and yet is vulnerable to capture by any piece except the soldier.",
-"RULES_SLIDE4":"A cannon captures as in xiangqi: it moves any distance along a single row or column of the board, jumping over exactly one intermediate piece (called a screen). Any other squares between the cannon and its target must be empty. The color of the screening piece does not matter; it may be friend or foe, or even face down. While a cannon may capture any piece, it must jump a screen to do so. Since a cannon must jump to capture, it cannot capture a piece in an adjacent square.",
-"RULES_SLIDE5":"Red has the 1st move. The player killed all pieces in other side win the game.",
+"RULES_OF_NNM":"Rules of Nine Men's Morris",
+"RULES_SLIDE1":"Nine Men's Morris has 3 phases, and each has different rules",
+"RULES_SLIDE2":"Phase1: Placing men on vacant points",
+"RULES_SLIDE3":"Phase2: Moving men to adjacent points",
+"RULES_SLIDE4":"Phase3: Moving men to any vacant point when a player has been reduced to three men",
+"RULES_SLIDE5":"Players try to form 'mills'— three of their own men lined horizontally or vertically",
+"RULES_SLIDE6":"When a 'mills' is formed you are allowed to remove an enemy's man.",
+"RULES_SLIDE7":"When one player loses has less than 3 players or has no place to move.",
 "CLOSE":"Close"
 });
